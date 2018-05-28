b5a6c5268376acb9e065f35b295725d29a093b83
keo-git <s.varezhkin.601@gmail.com>
2018-05-28 11:31:09 +0300

    fixed i/o

--- main.cpp
+++ main.cpp
@@ -2,19 +2,22 @@
 
 #include "src/chip8.h"
 
-int main() {
-  std::ifstream f("roms/pong", std::ios::binary | std::ios::ate);
-  uint16_t fsize = f.tellg();
+int main(int argc, char** argv) {
+  
+  std::ifstream f(argv[1], std::ios::binary | std::ios::ate);
+  int fsize = f.tellg();
   f.seekg(std::ios_base::beg);
 
   uint8_t* op = new uint8_t[fsize];
+  f.read(reinterpret_cast<char *>(op), fsize);
   for (uint16_t i = 0; i < fsize; i++) {
-    f >> op[i];
+    printf("%02x ", op[i]);
   }
 
   chip8 emulator(op, fsize);
   emulator.run_disassembler();
 
   delete []op;
+
   return 0;
 }
