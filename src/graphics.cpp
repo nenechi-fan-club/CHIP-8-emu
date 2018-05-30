32b71d6abe459622b1b72a5c441a9059587f4cbe
keo-git <s.varezhkin.601@gmail.com>
2018-05-30 11:38:52 +0300

    Testing new pixel scaling method

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -27,22 +27,25 @@ bool graphics::init() {
   texture = SDL_CreateTexture(renderer, 
     SDL_PIXELFORMAT_ARGB8888, 
     SDL_TEXTUREACCESS_STATIC, 
-    WINDOW_WIDTH, WINDOW_HEIGHT);
+    CHIP8_WIDTH, CHIP8_HEIGHT);
   if (texture == nullptr) {
     printf("Failed to create texture: %s", SDL_GetError());
     return false;
   }
 
+  SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, 0);
+
   return true;
 }
 
 int graphics::update_texture(uint32_t *pixel_buffer) {
-  return SDL_UpdateTexture(texture, nullptr, pixel_buffer, WINDOW_WIDTH * sizeof(uint32_t));
+  return SDL_UpdateTexture(texture, nullptr, pixel_buffer, CHIP8_WIDTH * sizeof(uint32_t));
 }
 
 void graphics::render() {
   SDL_RenderClear(renderer);
-  SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+  SDL_Rect rect = {.x = 0, .y = 0, .w = WINDOW_WIDTH, .h = WINDOW_HEIGHT};
+  SDL_RenderCopy(renderer, texture, nullptr, &rect);
   SDL_RenderPresent(renderer);
 }
 
